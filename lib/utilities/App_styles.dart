e31f6dacd5192bf6b69b1be4846759440ddfed59
Mohamed Ehab <mody36392@gmail.com>
2026-07-22 15:20:38 +0300

    + Added splash screen. + added intro screen

---
+++ lib/utilities/App_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/utilities/App_Colors.dart';
+
+class AppStyles {
+  static const TextStyle gold24bold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primaryColor,
+  );
+}
