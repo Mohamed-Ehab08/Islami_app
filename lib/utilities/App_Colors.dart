a72116ba973c177851020923f9450c4f08e7fbeb
Mohamed Ehab <mody36392@gmail.com>
2026-07-23 15:01:15 +0300

    + edited intro screen.

--- lib/utilities/App_Colors.dart
+++ lib/utilities/App_Colors.dart
@@ -3,4 +3,5 @@ import 'dart:ui';
 class AppColors {
   static const Color bgColor = Color(0xFF202020);
   static const Color primaryColor = Color(0xFFE2BE7F);
+  static const Color lightGreyColor = Color(0xFF707070);
 }
